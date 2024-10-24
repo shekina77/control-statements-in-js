f1d266bb414d764744a0063c8dc3752c777df7fe
unknown <shekinadhanunjay@gmail.com>
2024-10-24 12:24:17 +0530

    control statements in js

---
+++ control stat.js
@@ -0,0 +1,41 @@
+//for loop
+//pre increment
+for( var a=7;a < 12;a++){
+    console.log(a)
+}
+
+//post decrement
+for( var a=11;a > 6 ; a--){
+    console.log(a)
+}
+
+// while loop
+//pre increment
+var a=1;
+while(a <= 7){
+    console.log(a)
+    a++
+}
+//post decrement
+var a=10;
+while(a > 6){
+    console.log(a)
+    a--
+}
+
+//do while
+//pre increment
+var a=1
+do{
+    a++
+    console.log(a)
+
+}
+while(a <= 6)
+
+    //post decrement
+    var a=7;
+    do{
+        a--
+        console.log(a)
+    }while(a >= 3)
